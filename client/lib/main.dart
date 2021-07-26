3194298b4c6cbf11a761a7f0ca4bcd57571681db
Emmanuel Sackey <sackeynuel@gmail.com>
2021-07-26 21:04:34 +0000

    initial commit

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,17 +1,9 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:softdoc/cubit/android_nav_cubit/AndroidNav_cubit.dart';
 import 'package:softdoc/cubit/auth_cubit/auth_cubit.dart';
 import 'package:softdoc/platformSelect.dart';
-import 'package:softdoc/screens/auth_screen/auth_screen.dart';
-import 'package:softdoc/screens/desktop_screen/desktop_auth_screen.dart';
-import 'package:softdoc/screens/detail_screen/detail_screen.dart';
-import 'package:softdoc/screens/send_doc_screen/send_doc_screen.dart';
 import 'cubit/desktop_nav_cubit/desktopnav_cubit.dart';
-import 'style.dart';
-// import 'package:softdoc/screens/home_screen/home_screen.dart';
 
 void main() {
   runApp(MyApp());
