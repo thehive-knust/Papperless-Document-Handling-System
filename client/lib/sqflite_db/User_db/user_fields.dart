4e6530233a65200938f0771beb3a486849f39c58
cocastic8590 <cocasticmustdo@gmail.com>
2021-07-27 18:35:19 -0700

    create user data fields file

---
+++ client/lib/sqflite_db/User_db/user_fields.dart
@@ -0,0 +1,11 @@
+final String tableUser = 'users';
+
+class UserFields {
+  static final List<String> values = [
+    // Add documents fields
+    id, name,
+  ];
+
+  static final String id = '_id';
+  static final String name = 'name';
+}
