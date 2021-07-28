09cd9df8de840a53faaa9a00680f37ce8bf727b9
cocastic8590 <cocasticmustdo@gmail.com>
2021-07-27 18:29:10 -0700

    create document fields

---
+++ client/lib/sqflite_db/Document_db/document_fields.dart
@@ -0,0 +1,17 @@
+final String tableDocuments = 'documents';
+
+class DocumentFields {
+  static final List<String> values = [
+    // Add documents fields
+    id, subject, filepath, description, status, createdAt,  updatedAt,
+  ];
+
+  static final String id = '_id';
+  //static final String user_id = 'user_id';
+  static final String subject = 'subject';
+  static final String filepath = 'filepath';
+  static final String description = 'description';
+  static final String status = 'status';
+  static final String createdAt = 'createdAt';
+  static final String updatedAt = 'updatedAt';
+}
