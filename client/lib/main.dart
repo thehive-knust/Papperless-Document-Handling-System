ed7fd5b175c10cd5262358f85aeac865905800e3
cocastic8590 <cocasticmustdo@gmail.com>
2021-07-27 18:02:00 -0700

    delete local db

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,12 +1,5 @@
-import 'package:flutter/material.dart';
-import 'package:softdoc/platformSelect.dart';
-import 'package:softdoc/screens/auth_screen/auth_screen.dart';
-import 'package:softdoc/screens/desktop_screen/desktop_auth_screen.dart';
-import 'package:softdoc/screens/detail_screen/detail_screen.dart';
-import 'package:softdoc/screens/send_doc_screen/send_doc_screen.dart';
-import 'style.dart';
-// import 'package:softdoc/screens/home_screen/home_screen.dart';
 
+import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
