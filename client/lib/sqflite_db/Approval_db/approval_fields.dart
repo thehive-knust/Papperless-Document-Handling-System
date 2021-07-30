5283c9174716d99921958d85705a21c47e8cc47c
cocastic8590 <cocasticmustdo@gmail.com>
2021-07-30 16:14:36 -0700

    add data fields for approval table

---
+++ client/lib/sqflite_db/Approval_db/approval_fields.dart
@@ -0,0 +1,11 @@
+final String tableUser = 'users';
+
+class ApprovalFields {
+  static final List<String> values = [
+    // Add documents fields
+    id, status,
+  ];
+
+  static final String id = '_id';
+  static final String status = 'status';
+}
