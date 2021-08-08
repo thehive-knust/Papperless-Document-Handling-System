b94ea802dcdb699553b6acf5409adf41311941f5
Koffi Cobbin <52169278+Koffi-Cobbin@users.noreply.github.com>
2021-08-08 12:21:38 +0000

    Update approval_routes.py

--- src/pdhs_app/blueprints/approval_routes.py
+++ src/pdhs_app/blueprints/approval_routes.py
@@ -19,3 +19,64 @@ def update():
         approval.status = status
         approval.save_to_db()
         return {"message": "Done"}
+
+
+@bp.route('/', methods=['GET'])
+def get_all_approvals():
+    """
+    Get all the approvals in the approval
+    table.
+    """
+    if request.method == 'GET':
+        approvals = []
+        result = []
+        error_msg = None
+        try:
+            result = Approval.query.all()
+        except:
+            error_msg = 'Error occured retrieving approvals'
+        if len(result) == 0:
+            error_msg = 'No approvals available'
+        if error_msg is not None:
+            return jsonify(msg=error_msg)
+        else:
+            for approval in result:
+                approvals.append(approval.to_json())
+            return jsonify(approvals=approvals)
+
+
+@bp.route('/<int:approval_id>', methods=['GET'])
+def get_approval_by_id(approval_id):
+    """
+    Get a particular approval by id
+    """
+    if request.method == 'GET':
+        error_msg = None
+        try:
+            approval = Approval.find_by_id(approval_id)
+            if approval is None:
+                error_msg = f'No approval with ID {approval_id} found'
+        except:
+            error_msg = 'Error occured finding approval'
+        if error_msg is not None:
+            return jsonify(msg=error_msg), 404
+        elif approval is not None:
+            return jsonify(approval.to_json())
+
+@bp.route('/delete/<int:approval_id>', methods=['DELETE'])
+def delete_approval(approval_id):
+    if request.method == 'DELETE':
+        error_msg = None
+        try:
+            approval = Approval.find_by_id(approval_id)
+        except:
+             error_msg = 'Error occured finding approval'
+        if approval is not None:
+            try:
+                approval.delete_from_db()
+            except:
+                error_msg = 'Error occured deleting Approval'
+        if error_msg is not None:
+            return jsonify(msg=error_msg), 404
+        else:
+            return jsonify(msg='Approval deleted successfully')
