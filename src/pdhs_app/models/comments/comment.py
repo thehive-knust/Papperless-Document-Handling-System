39f55cf6ddd1fda54787359c63a2df77fd911e8a
Koffi Cobbin <52169278+Koffi-Cobbin@users.noreply.github.com>
2021-08-08 12:27:43 +0000

    Update comment.py

--- src/pdhs_app/models/comments/comment.py
+++ src/pdhs_app/models/comments/comment.py
@@ -1,32 +1,69 @@
 from src.database.db import db
+from datetime import datetime
 
 
-class Comment(db.Model):
+class Document(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    document_id = db.Column(db.Integer, db.ForeignKey('document.id'), nullable=False)
-    content = db.Column(db.String(1000), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    name = db.Column(db.String(120), nullable=False)
+    subject = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(255), nullable=True)
+    file = db.Column(db.String(255), nullable=False) #file_path
+    progress = db.Column(db.String(50), nullable=False, default='Pending')
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    approvals = db.relationship('Approval', backref='document', lazy='joined')
+    comment = db.relationship('Comment', backref='document', lazy='joined', uselist=False)
 
     def __repr__(self):
-        return '<Comment on %r>' % self.document_id
+        return '<Document %r>' % self.name
+
+    @classmethod
+    def find_by_name(cls, name):
+        """
+        Query the table represented by this model
+        by the name column.
+        :return: A Document object or None
+        """
+        return cls.query.filter_by(name=name).first()
 
     @classmethod
     def find_by_id(cls, id):
+        """
+        Query the table represented by this model
+        by the id column.
+        :return: A Document object or None
+        """
         return cls.query.get(id)
 
     def save_to_db(self):
+        """
+        Save a record represented by this model object 
+        into its table in the database.
+        """
         db.session.add(self)
         db.session.commit()
 
     def delete_from_db(self):
+        """
+        Delete the record represented by this model object, 
+        from its the table in the database.
+        """
         db.session.delete(self)
         db.session.commit()
 
     def to_json(self):
-        comment = {
+        """
+        Convert the model into a json serializable object.
+        :return: a dictionary that maps table columns to their values
+        """
+        doc = {
             'id': self.id,
-            'sender_id': self.sender_id,
-            'document_id': self.document_id,
-            'content': self.content
+            'user_id': self.user_id,
+            'name': self.name,
+            'subject': self.subject,
+            'file': self.file, #file_path
+            'description': self.description,
+            'progress': self.progress,
         }
-        return comment
+        return doc
