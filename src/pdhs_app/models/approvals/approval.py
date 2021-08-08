11dd64528ff4f80bfbc43f9c4a9e7d512b261506
Koffi Cobbin <52169278+Koffi-Cobbin@users.noreply.github.com>
2021-08-08 12:26:06 +0000

    Update approval.py

--- src/pdhs_app/models/approvals/approval.py
+++ src/pdhs_app/models/approvals/approval.py
@@ -2,7 +2,7 @@ from src.database.db import db
 from src.pdhs_app.models.approvals import errors as ApprovalErrors
 
 class Approval(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     document_id = db.Column(db.Integer, db.ForeignKey('document.id'), nullable=False)
     recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     status = db.Column(db.Boolean, default=False)
