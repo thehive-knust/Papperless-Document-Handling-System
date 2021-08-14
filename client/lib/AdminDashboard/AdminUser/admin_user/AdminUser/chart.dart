5d4143623edc03c39071af5017484ff207a52f0c
cocastic8590 <cocasticmustdo@gmail.com>
2021-08-13 21:48:10 -0700

    create admin user file

---
+++ client/lib/AdminDashboard/AdminUser/admin_user/AdminUser/chart.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+
+
+class Chart extends StatefulWidget {
+  const Chart({Key? key}) : super(key: key);
+
+  @override
+  _ChartState createState() => _ChartState();
+}
+
+
+class _ChartState extends State<Chart> {
+  List<GDPData> chartData=getChartData();
+
+  @override
+  Widget build(BuildContext context) {
+    return SfCartesianChart(
+      isTransposed: true,
+      series: <ChartSeries>[
+        BarSeries<GDPData, String>(
+          dataSource: chartData,
+          xValueMapper: (GDPData gdp, _) => gdp.continent,
+          yValueMapper: (GDPData gdp, _) => gdp.gdp,
+        )
+      ],
+      primaryXAxis: CategoryAxis(),
+    );
+  }
+}
+
+List<GDPData> getChartData(){
+  final List<GDPData> chartData=[
+    GDPData("Student Users", 50),
+    GDPData("Staff Users", 200),
+  ];
+  return chartData;
+}
+
+class GDPData {
+  GDPData(this.continent, this.gdp);
+  final String  continent;
+  final  double gdp;
+}
