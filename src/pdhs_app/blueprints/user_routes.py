4df9b0eb29d8402e9d2cfe4ea5782ae5f97f4579
Koffi Cobbin <52169278+Koffi-Cobbin@users.noreply.github.com>
2021-08-17 10:31:34 +0000

    Update user_routes.py

--- src/pdhs_app/blueprints/user_routes.py
+++ src/pdhs_app/blueprints/user_routes.py
@@ -6,8 +6,18 @@ import src.pdhs_app.models.users.constants as UserConstants
 from src.pdhs_app.models.documents.document import Document
 from src.pdhs_app.models.departments.department import Department
 from src.pdhs_app.blueprints.document_routes import inbox as get_new_docs
+from werkzeug.utils import secure_filename
+from storage.cloud_storage import delete_blob, upload_blob
 
 bp = Blueprint('users', __name__, url_prefix='/users')
+
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+
+def _allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @bp.route('/<int:user_id>', methods=['GET'])
@@ -31,24 +41,6 @@ def get_user_by_email(email):
     return jsonify(msg="User not found"), 404
 
 
-@bp.route('delete/<int:user_id>', methods=['DELETE'])
-def delete(user_id):
-    if request.method == 'DELETE':
-        user = User.find_by_id(user_id)
-    if user is not None:
-        try:
-            user.delete_from_db()
-        except:
-            return jsonify(msg="Error deleting user."), 500
-
-    return jsonify(msg="User not found"), 404
-
-
-@bp.route('/update/<int:user_id>', methods=['POST'])
-def update_user(user_id):
-    pass
-
-
 @bp.route('/', methods=['GET'])
 def get_all_users():
     """
@@ -66,9 +58,70 @@ def get_all_users():
         if len(users) == 0 or len(result) == 0:
             return jsonify({'msg': 'Ther are no registered users'}), 404
         return jsonify({'users': users})
+    
 
+@bp.route('/update/profile-image/<int:user_id>', methods=['PUT'])
+def update_user_profile_image(user_id):
+    """
+    Handling the upload of a user profile image.
+    """
+    if request.method == 'PUT':
+        image_file = request.files.get('image', None)
+        error_msg = None
+        if user_id is None:
+            error_msg = 'User ID is required'
+        elif image_file is None:
+            error_msg = 'No selected file'
+        if error_msg is not None:
+            return jsonify(msg=error_msg), 500
+        else:
+            user = User.find_by_id(user_id)
+            if user is None:
+                return jsonify(msg="Unauthorized request"), 401
+
+        if _allowed_file(image_file.filename):
+            filename = secure_filename(image_file.filename)
+            image_filename = f"{user_id}_{filename}"
+            image_url = upload_blob(
+                image_file.stream, image_filename)
+            if image_url is not None:
+                user.profile_image_link = image_url
+            else:
+                return jsonify(msg="Error uploading image")
+            try:
+                user.save_to_db()
+                return jsonify(image=image_url), 200
+            except:
+                return jsonify(msg='Error updating profile'), 500
+        else:
+            return jsonify(msg="File type not supported"), 500
+
+
+@bp.route('/delete/profile-image/<int:user_id>', methods=['DELETE'])
+def delete_user_profile_image(user_id):
+    if request.method == 'DELETE':
+        error_msg = None
+        try:
+            user = User.find_by_id(user_id)
+        except:
+            error_msg = 'Error occured finding user'
+            error_code = 404
+        if user is not None:
+            image_filename = user.profile_image_link.split('/').pop()
+            if not delete_blob(image_filename):
+                error_msg = 'Error occured deleting user from cloud storage'
+                error_code = 500
+            else:
+                user.profile_image_link = ''
+                user.save_to_db()
+        if error_msg is not None:
+            return jsonify(msg=error_msg), error_code
+        else:
+            return jsonify(msg='Profile Image deleted successfully'), 200
+        
+        
 @bp.route('delete/<int:user_id>', methods=['DELETE'])
-def delete_user(user_id):
+def delete(user_id):
     if request.method == 'DELETE':
         user = User.find_by_id(user_id)
     if user is not None:
